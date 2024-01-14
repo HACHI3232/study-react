57f7f8cb30233eacf79fe0884d8f6528efd28e14
HACHI3232 <harada.chisato526@gmail.com>
2024-01-14 23:24:41 +0900

    💄 スタイルファイルの分割

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,6 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 
-
 export default function Home() {
   return (
     <div className={styles.container}>
